1821b61da2c8d997f3b3aa4973e80404dd51d8ea
David Cramer <dcramer@gmail.com>
2013-08-13 23:33:10 -0700

    Simple initial data and factory concepts

--- src/sentry/static/sentry/scripts/controllers.js
+++ src/sentry/static/sentry/scripts/controllers.js
@@ -1,12 +1,25 @@
 // TODO(cramer): this should be defined elsewhere
 var Sentry = angular.module('Sentry', []);
 
-Sentry.controller('StreamCtrl', ['$scope', '$window', function($scope, $window){
-	$scope.groups = $window.groups;
-	// TODO(cramer): figure out how to better define utilities in Angular apps.\
-	$scope.utils = app.utils;
+Sentry.factory('GroupModel', function() {
+    function GroupModel(initialData) {
+        var self = this;
+        // Just deep copy constructor data onto this object
+        angular.copy(initialData, self);
 
-	$scope.getLevelClassName = function(group){
-        return 'level-' + group.levelName;
+        self.getLevelClassName = function() {
+            return 'level-' + self.levelName;
+        };
     };
+
+    return GroupModel;
+});
+
+Sentry.controller('StreamCtrl', ['$scope', 'initial', 'GroupModel', function($scope, initial, GroupModel){
+    $scope.groups = initial.groups.map(function(val) {
+        return new GroupModel(val);
+    });
+
+    // TODO(cramer): figure out how to better define utilities in Angular apps.\
+    $scope.utils = app.utils;
 }]);
